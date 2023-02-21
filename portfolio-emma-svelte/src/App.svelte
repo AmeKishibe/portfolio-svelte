f6ff659dcf144c21896477c19071b307888eafff
AmeKishibe <emma.guillaume@mmibordeaux.com>
2023-02-21 22:04:47 +0100

    first commit

---
+++ portfolio-emma-svelte/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Header from "./components/Header.svelte";
+</script>
+<Header />
+<main>
+
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sit, reprehenderit sequi similique ea voluptatibus nemo asperiores et cupiditate temporibus cumque explicabo fugit vel praesentium aspernatur maxime, pariatur aperiam? Impedit.</p>
+</main>
+
+<style>
+	main{
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
---
+++ portfolio-emma-svelte/src/components/Header.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+
+</script>
+
+<header class="header">
+    <div class="header__title"><a href="#"><h1>Emma Guillaume Portfolio</h1></a><div class="header__title--underline"></div></div>
+    <div class="buttons__header">
+        <a href="#">CV</a>
+        <a href="#">Contact</a>
+    </div>
+</header>
+
+<style>
+    .header__title--underline{
+        background-color: #79A671;
+        width: 100%;
+        height: 5px;
+
+    }
+    .header{
+        padding: 2vh 10vw;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .buttons__header{
+        display: flex;
+        gap: 5vw;
+    }  
+    @keyframes myanimation {
+            from {color: #303D21;
+                border: solid 2px #303D21;
+                background-color: #ffffff00;
+            }
+            to {color: white;
+                border: solid 2px #79A671;
+                background-color: #79A671;
+            }
+        }
+        @keyframes reverse {
+            from {color: white;
+                border: solid 2px #79A671;
+                background-color: #79A671;
+            }
+            to {
+                color: #303D21;
+                border: solid 2px #303D21;
+                background-color: #ffffff00;
+            }
+        }
+        .buttons__header a{
+        animation-name: reverse;
+        animation-duration: 0.15s;
+        font-weight: 600;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        color: #303D21;
+        border: solid 2px #303D21;
+        border-radius: 30px;
+        padding: 1vh 2vw;
+    }
+        .buttons__header a:hover{
+        animation-name: myanimation;
+        animation-duration: 0.15s;
+        animation-fill-mode: forwards;
+    }
+</style>
